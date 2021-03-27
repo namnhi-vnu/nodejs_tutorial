cb93c6f6e2e25250d355d4e8a785ab4361ac4fa6
namnhi997 <namnhi.vnu@gmail.com>
2021-03-27 16:22:40 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
+
+app.use(morgan('combined'))
+
 // định nghĩa đường dẫn
 app.get('/tin-tuc', (req, res) => {
     res.send('Hello World! Nam Nhi')
